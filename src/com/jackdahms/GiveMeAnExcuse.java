d61442bb0aba3f92ee34e895acecff7e7b1cf180
Deco <jack.dahms@gmail.com>
2015-03-09 00:06:02 -0500

    hrmm

--- src/com/jackdahms/GiveMeAnExcuse.java
+++ src/com/jackdahms/GiveMeAnExcuse.java
@@ -1,7 +1,17 @@
 package com.jackdahms;
 
-import java.io.File;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 import java.util.Random;
@@ -15,12 +25,131 @@ import javax.mail.Store;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.UIManager;
+import javax.swing.border.Border;
 
 public class GiveMeAnExcuse {
 	
-	private static String USER_NAME = "givemeanexcuse@gmail.com"; 
+	public static final int INFO = 0,
+							COMMAND = 1,
+							SEVERE = 2;
 	
+	private static String USER_NAME = "givemeanexcuse@gmail.com"; 
+	private static JTextArea display;
+		
+	private static int WIDTH = 850, HEIGHT = 550;
+		
     public static void main(String[] args) throws Exception{
+    	
+    	Arrays.sort(args);
+    	if (Arrays.binarySearch(args, "nogui") != -1)
+    		;
+    	else
+    		createAndShowGUI();
+    	
+    	start();
+    }
+    
+    public static void createAndShowGUI() throws Exception{
+    	//set look and feel
+    	UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    	//set up jframe
+    	JFrame frame = new JFrame();
+    	
+    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	frame.setSize(WIDTH, HEIGHT);
+    	frame.setLocationRelativeTo(null);
+    	frame.setResizable(false);
+    	
+    	Container pane = frame.getContentPane();
+    	pane.setLayout(new BoxLayout(pane, BoxLayout.X_AXIS));
+    	
+    	int statWidth = WIDTH / 4;
+    	
+    	JPanel stats = new JPanel();
+    	stats.setBorder(BorderFactory.createTitledBorder("Connected this session")); //TODO  rename title to "stats" once you've thought of some stats
+    	stats.setMaximumSize(new Dimension(statWidth, HEIGHT));
+    	stats.setLayout(new BoxLayout(stats, BoxLayout.Y_AXIS));
+    	
+    	JPanel numbers = new JPanel();
+    	
+    	JPanel session = new JPanel();
+    	session.setBorder(BorderFactory.createTitledBorder("Session"));
+    	session.setLayout(new GridLayout(1, 1));
+    	
+    	JTextArea players = new JTextArea();
+    	players.setFont(new Font("courier", Font.PLAIN, 12));
+    	players.setLineWrap(true);
+    	players.setWrapStyleWord(true);
+    	players.setEditable(false);
+    	JScrollPane playScroll = new JScrollPane(players);
+    	playScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+    	
+//    	session.add(playScroll); //TODO uncomment these lines once you've thought of some stats
+    	
+//    	stats.add(numbers);
+//    	stats.add(session);
+    	
+    	stats.add(playScroll);
+    	
+    	JPanel log = new JPanel();
+    	log.setBorder(BorderFactory.createTitledBorder("Log"));
+    	log.setMaximumSize(new Dimension(WIDTH - statWidth, HEIGHT));
+    	log.setLayout(new BorderLayout());
+    	
+    	display = new JTextArea();
+    	display.setFont(new Font("courier", Font.PLAIN, 12));
+    	display.setLineWrap(true);
+    	display.setWrapStyleWord(true);
+    	display.setEditable(false);
+    	JScrollPane dispScroll = new JScrollPane(display);
+    	dispScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+    	
+    	
+    	JTextField comm = new JTextField();
+    	comm.setFont(new Font("courier", Font.PLAIN, 12));
+    	comm.addActionListener(new ActionListener(){
+    		public void actionPerformed(ActionEvent e) {
+    			command(comm.getText());
+    			comm.setText("");
+    		}
+    	});
+    	
+    	log.add(dispScroll, BorderLayout.CENTER);
+    	log.add(comm, BorderLayout.SOUTH);
+    	
+    	pane.add(stats);
+    	pane.add(log);
+    	
+    	frame.setVisible(true);
+    }
+    
+    public static void command(String msg) {
+    	
+    	append(1, msg);
+    }
+    
+    public static void append(String msg) {
+    	append(0, msg);
+    }
+    
+    public static void append(int code, String msg) {
+    	switch(code) {
+    		case COMMAND: display.append("[COMMAND] "); break;
+    	}
+    	display.append(msg + "\n");
+    }
+    
+    public static void start() throws Exception{
     	//things that only need to be set once
         String from = USER_NAME;
         List<String> excuses = new ArrayList<String>();
@@ -33,12 +162,12 @@ public class GiveMeAnExcuse {
         Random gen = new Random();
         
         //read password
-        Scanner	key = new Scanner(new File("key.lock"));
+        Scanner	key = new Scanner(GiveMeAnExcuse.class.getResourceAsStream("key.lock"));
         pass = key.nextLine();
         key.close();
         
         //read excuses
-        Scanner excuse = new Scanner(new File("excuses.txt"));
+        Scanner excuse = new Scanner(GiveMeAnExcuse.class.getResourceAsStream("excuses.txt"));
         while (excuse.hasNextLine())
         	excuses.add(excuse.nextLine());
         excuse.close();
